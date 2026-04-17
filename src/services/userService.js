c5732c736c7bfd13c249ec341e2030679f7ba439
LucaryDev <242092893+LucaryDev@users.noreply.github.com>
2026-04-16 21:15:36 -0300

    feat: fetch users without password

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,10 +1,12 @@
-const getUsers = () => {
-  return [
-    { 
-      id: 1,
-      name: "lucas", 
-      email: "lucas@gmail.com"
-    }]
+const User = require("../models/User")
+
+const getUsers = async () => {
+  const users = await User.findAll()
+  
+  return users.map((user) => {
+   const { password, ...rest } = user.toJSON()
+   return rest
+  })
 }
 
 module.exports = {
