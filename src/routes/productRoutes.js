63388e064e6fd7bcf64b0e07143f59dc970f5b1a
DevLucary <lucarydev1@gmail.com>
2026-04-29 20:42:47 -0400

    feat: protect productRoutes with authMiddleware

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getAllProducts, getProductsById, createNewProduct, updateProducts, deleteProducts } = require('../controllers/productController')
-
+const { authMiddleware } = require('../middlewares/authMiddleware')
 router.get('/', getAllProducts)
 router.get('/:id', getProductsById)
-router.post('/', createNewProduct)
-router.put('/:id', updateProducts)
-router.delete('/:id', deleteProducts)
+router.post('/', authMiddleware, createNewProduct)
+router.put('/:id', authMiddleware, updateProducts)
+router.delete('/:id', authMiddleware, deleteProducts)
 
 module.exports = router;
