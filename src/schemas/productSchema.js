4988bff7f420cdb787c4485648e4bb5dcec8ad5d
DevLucary <lucarydev1@gmail.com>
2026-04-29 20:42:04 -0400

    feat: add zod validation of stock in createProductSchema

--- src/schemas/productSchema.js
+++ src/schemas/productSchema.js
@@ -6,7 +6,8 @@ const createProductSchema = z.object({
     description: z.string().optional(),
     price: z.number().positive("price it must be a positive number"),
     categoryId: z.number().int("category ID must be a number integer").positive("category ID must be a positive number"),
-    image: z.string().url().optional()
+    image: z.string().url().optional(),
+    stock: z.number().int("stock must be a number integer").min(0, "stock must be a positive number").positive("stock must be a positive number").optional()
 })
 
 const updateProductSchema = createProductSchema.partial()
