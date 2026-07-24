f4877f5d9c36cbb52bbd1c6644aa1caaf48693fa
Lucary Leão Ferreira <lucarydev1@gmail.com>
2026-07-24 09:54:52 -0300

    fix(config): add a pool to manage MySQL connections.

--- src/config/db.js
+++ src/config/db.js
@@ -12,7 +12,13 @@ if (process.env.DB_DIALECT === "sqlite") {
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 1,
+      acquire: 30000,
+      idle: 10000
+    }
   })
 
 }
