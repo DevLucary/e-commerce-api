4ef9ed9d645ef4006f0c498656fbd4c0e4182831
Lucary Leão Ferreira <lucarydev1@gmail.com>
2026-07-24 17:53:21 -0300

    test(products): add sorting, filtering and pagination tests

--- tests/products.test.js
+++ tests/products.test.js
@@ -9,6 +9,8 @@ describe("Products", () => {
     })
 
     let token
+    let categoryId
+    let categoryId2
 
     it("Deve criar o usuário", async () => {
         const response = await request.post("/users").send({
@@ -33,8 +35,21 @@ describe("Products", () => {
 
     it("deve criar uma categoria", async () => {
         const response = await request.post("/categories").set("Authorization", `Bearer ${token}`).send({
-            name: "Teste"
+            name: "Eletrônicos"
         })
+
+        categoryId = response.body.id
+        
+        
+        expect(response.status).toBe(201)
+    })
+
+    it("Deve criar outra categoria", async () => {
+        const response = await request.post("/categories").set("Authorization", `Bearer ${token}`).send({
+            name: "Roupas"
+        })
+        
+        categoryId2 = response.body.id
         
         expect(response.status).toBe(201)
     })
@@ -44,16 +59,180 @@ describe("Products", () => {
         
         expect(response.status).toBe(200)
     })
-
-    it("Deve criar um novo produto", async () => {
-        const response = await request.post("/products").set("Authorization", `Bearer ${token}`).send({
-            title: "Teste",
-            price: 100,
-            categoryId: 1,
-            stock: 10
-        }) 
+    
+    it("Deve criar novos produtos", async () => {
         
-        expect(response.status).toBe(201)
+        const products = [
+            {
+                title: "Mouse RGB",
+                price: 100,
+                categoryId: categoryId,
+                stock: 10
+            },
+            {
+                title: "Teclado RGB",
+                price: 50,
+                categoryId: categoryId,
+                stock: 20
+            },
+            {
+                title: "Camiseta",
+                price: 70,
+                categoryId: categoryId2,
+                stock: 50
+            },
+            {
+                title: "Calça",
+                price: 20,
+                categoryId: categoryId2,
+                    stock: 100
+            },
+            {
+                title: "Tênis",
+                price: 300,
+                categoryId: categoryId2,
+                stock: 30
+            },
+            {
+                title: "Monitor",
+                price: 500,
+                categoryId: categoryId,
+                stock: 40
+            }
+        ]
+
+        for (const p of products) {
+           const response = await request.post('/products')
+                .set('Authorization', 'Bearer ' + token)
+                .send(p)
+
+                expect(response.status).toBe(201)
+        }
+        
+    })
+    
+    it("Deve ordenar produtos por preço crescente", async () => {
+        const response = await request.get("/products?sort=price&order=asc")
+    
+        expect(response.status).toBe(200)
+
+        const products = response.body.products
+    
+        const prices = products.map(p => Number(p.price))
+    
+        for (let i = 0; i < prices.length - 1; i++) {
+
+            expect(prices[i]).toBeLessThanOrEqual(prices[i + 1])
+        }
+    })
+
+    it("Deve ordenar produtos por preço decrescente", async () => {
+        const response = await request.get("/products?sort=price&order=desc")
+    
+        expect(response.status).toBe(200)
+
+        const products = response.body.products
+    
+        const prices = products.map(p => Number(p.price))
+    
+        for (let i = 0; i < prices.length - 1; i++) {
+
+            expect(prices[i]).toBeGreaterThanOrEqual(prices[i + 1])
+        }
+    })
+
+    it("Deve retornar a primeira página com 3 produtos", async () => {
+        const response = await request.get("/products?page=1&limit=3")
+        
+        expect(response.status).toBe(200)
+        
+        const products = response.body.products
+        
+        expect(products.length).toBe(3)
+        expect(response.body.pagination.page).toBe(1)
+        expect(response.body.pagination.limit).toBe(3)
+        expect(response.body.pagination.total).toBe(6)
+        expect(response.body.pagination.totalPages).toBe(2)
+    })
+
+    it("Deve retornar a segunda página com 3 produtos", async () => {
+        const response = await request.get("/products?page=2&limit=3")
+        
+        expect(response.status).toBe(200)
+        
+        const products = response.body.products
+        
+        expect(products.length).toBe(3)
+        expect(response.body.pagination.page).toBe(2)
+        expect(response.body.pagination.limit).toBe(3)
+        expect(response.body.pagination.total).toBe(6)
+        expect(response.body.pagination.totalPages).toBe(2)
+    })
+
+    it("Deve retornar produtos com a categoria eletrônicos", async () => {
+        const response = await request.get(`/products?category=${categoryId}`)
+        
+        expect(response.status).toBe(200)
+        
+        const products = response.body.products
+        
+        expect(products.length).toBe(3)
+        expect(products.every(p => p.categoryId === categoryId)).toBe(true);
+    })
+
+    it("Deve retornar produtos com a categoria roupas", async () => {
+        const response = await request.get(`/products?category=${categoryId2}`)
+        
+        expect(response.status).toBe(200)
+        
+        const products = response.body.products
+        
+        expect(products.length).toBe(3)
+        expect(products.every(p => p.categoryId === categoryId2)).toBe(true);
+    })
+
+    it("Deve retornar produtos que tenham o filtro do título", async () => {
+        const response = await request.get("/products?title=RGB")
+        
+        expect(response.status).toBe(200)
+        
+        const products = response.body.products
+        
+        expect(products.length).toBe(2)
+        expect(products.every(p => p.title.includes("RGB"))).toBe(true);
+    })
+
+    it("Deve retornar produtos com o filtro de preço mínimo", async () => {
+        const response = await request.get("/products?minPrice=100")
+        
+        expect(response.status).toBe(200)
+        
+        const products = response.body.products
+        
+        expect(products.length).toBe(3)
+        expect(products.every(p => p.price >= 100)).toBe(true);
+    })
+
+    it("Deve retornar produtos com o filtro de preço máximo", async () => {
+        const response = await request.get("/products?maxPrice=50")
+        
+        expect(response.status).toBe(200)
+        
+        const products = response.body.products
+        
+        expect(products.length).toBe(2)
+        expect(products.every(p => p.price <= 50)).toBe(true);
+    })
+
+    it("Deve retornar produtos com todos os filtros", async () => {
+        const response = await request.get(`/products?category=${categoryId}&title=RGB&minPrice=100&maxPrice=500`)
+        
+        expect(response.status).toBe(200)
+        
+        const products = response.body.products
+        
+        expect(products.length).toBe(1)
+        expect(products.every(p => p.categoryId === categoryId && p.title.includes("RGB") && p.price >= 100 && p.price <= 500)).toBe(true);
     })
 
     it("Deve retornar erro de autorização quando não fornecer token", async () => {
