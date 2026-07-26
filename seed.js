1ebee4e107215a65984afa7c5efcd7c446cbe121
Lucary Leão Ferreira <lucarydev1@gmail.com>
2026-07-26 12:55:41 -0300

    feat(seed): add admin seed script

---
+++ seed.js
@@ -0,0 +1,40 @@
+require("dotenv").config()
+const bcrypt = require("bcryptjs")
+const User = require("./src/models/User")
+const { sequelize } = require("./src/config/db")
+
+async function seed() {
+    await sequelize.sync()
+
+    const admin = await User.findOne({ where: { role: 'admin' } })
+
+    if(admin) {
+    console.log("Admin already exists")
+    return
+    }
+
+    const hashedPassword = await bcrypt.hash("123456", 10)
+
+    await User.create({
+        name: "Admin",
+        email: "admin@email.com",
+        password: hashedPassword,
+        role: "admin"
+    })
+
+    console.log("Admin created successfully")
+}
+
+seed().then(async () => {
+    console.log("Seed complete")
+
+    await sequelize.close()
+
+    process.exit(0)
+}).catch(async (error) => {
+    console.error(error)
+
+    await sequelize.close()
+
+    process.exit(1)
+})
