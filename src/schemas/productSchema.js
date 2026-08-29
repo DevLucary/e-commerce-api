cc921143022152610f3fff35b2c5c06c293447fd
Lucary Leão Ferreira <lucarydev1@gmail.com>
2026-08-29 18:28:23 -0300

    fix: allow zero product stock

--- src/schemas/productSchema.js
+++ src/schemas/productSchema.js
@@ -1,18 +1,24 @@
-const z = require('zod')
-const Category = require('../models/Category')
+const z = require("zod")
 
 const createProductSchema = z.object({
-    title: z.string().min(2, "title is required"),
-    description: z.string().optional(),
-    price: z.number().positive("price it must be a positive number"),
-    categoryId: z.number().int("category ID must be a number integer").positive("category ID must be a positive number"),
-    image: z.string().url().optional(),
-    stock: z.number().int("stock must be a number integer").min(0, "stock must be a positive number").positive("stock must be a positive number").optional()
+  title: z.string().min(2, "title is required"),
+  description: z.string().optional(),
+  price: z.number().positive("price must be a positive number"),
+  categoryId: z
+    .number()
+    .int("category ID must be an integer")
+    .positive("category ID must be a positive number"),
+  image: z.string().url().optional(),
+  stock: z
+    .number()
+    .int("stock must be an integer")
+    .min(0, "stock cannot be negative")
+    .optional()
 })
 
 const updateProductSchema = createProductSchema.partial()
 
 module.exports = {
-    createProductSchema,
-    updateProductSchema
+  createProductSchema,
+  updateProductSchema
 }
