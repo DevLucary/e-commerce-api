47a8d25165a4cda9d6d5a9eefe5ead3430632aff
Lucary Leão Ferreira <lucarydev1@gmail.com>
2026-08-29 20:18:51 -0300

    fix: support remote mysql port and ssl

--- src/config/db.js
+++ src/config/db.js
@@ -8,19 +8,34 @@ if (process.env.DB_DIALECT === "sqlite") {
     storage: ":memory:",
     logging: false
   })
-
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 1,
-      acquire: 30000,
-      idle: 10000
-    }
-  })
+  const useSSL = process.env.DB_SSL === "true"
 
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT || 3306),
+      dialect: "mysql",
+
+      dialectOptions: useSSL
+        ? {
+            ssl: {
+              rejectUnauthorized: true
+            }
+          }
+        : {},
+
+      pool: {
+        max: 5,
+        min: 1,
+        acquire: 30000,
+        idle: 10000
+      }
+    }
+  )
 }
 
 module.exports = {
