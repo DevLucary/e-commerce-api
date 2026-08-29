28b1f54010651b48dd27b12a93eab6ce392f4b8a
Lucary Leão Ferreira <lucarydev1@gmail.com>
2026-08-29 18:23:10 -0300

    fix: restrict user listing to admins

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,8 +2,9 @@ const express = require("express")
 const router = express.Router()
 const { getUsers, createUser } = require("../controllers/userController")
 const { authMiddleware } = require("../middlewares/authMiddleware")
+const adminMiddleware = require("../middlewares/adminMiddleware")
 
-router.get("/", authMiddleware, getUsers)
+router.get("/", authMiddleware, adminMiddleware, getUsers)
 
 router.post("/", createUser)
 
